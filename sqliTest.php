718376e07e30449ff6ed3089ab8913634ce6d297
globalvar <dwordnavneet@gmail.com>
2021-02-05 10:25:54 +0530

    sqlitest file

---
+++ sqliTest.php
@@ -0,0 +1,34 @@
+<?php
+
+$conn = mysqli_connect("localhost","dvwa","password","project");
+
+
+if(isset($_GET['username']) && isset($_GET['password']))
+{
+$username = $_GET['username'];
+$password = $_GET['password'];
+
+
+$q = "select * from users where username='$username' and password='$password' ";
+
+$result = mysqli_query($conn, $q);
+
+ while($row = mysqli_fetch_assoc($result)) {
+
+    echo "uid: " . $row["uid"]. " Username: " . $row["username"]. " password " . $row["password"]. " Name:".$row["name"]."<br>";
+  }
+
+}
+
+
+?>
+
+<html>
+<form action="" method="get">
+<input type='text' name='username'/>
+<input type='pass' name='password' />
+<input type='submit' value='submit' />
+</form>
+</html>
+
+
